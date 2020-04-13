5b1fbb4f67360fdc3cbb419efcaa50489d4e8291
BinaryKiwiz <45517110+BinaryKiwiz@users.noreply.github.com>
2020-04-13 10:16:36 -0700

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,35 @@
+var CurrentPi = 0;
+var CurrentFractionPi = 1;
+var Iteration = 0;
+var Denominator = 1;
+
+function DefinePi(){
+  CurrentPi = 4 * CurrentFractionPi;
+  CurrentPi = CurrentPi.toPrecision(50);
+}
+
+function RefreshPi(){
+  document.getElementById("Pi").innerHTML = CurrentPi;
+}
+
+function RefreshIteration(){
+  document.getElementById("Iteration").innerHTML = Iteration;
+}
+
+function Iterate(){
+  Denominator += 2
+  CurrentFractionPi -= 1 / Denominator;
+  Iteration += 1;
+  DefinePi();
+  
+  Denominator += 2
+  CurrentFractionPi += 1 / Denominator;
+  Iteration += 1;
+  DefinePi();
+}
+
+while(Iteration < 1000000){
+  Iterate();
+  RefreshPi();
+  RefreshIteration();
+}
